18b5dbfec8e7c0706b25d487365a27c616c7d704
Zenya <mm_9290@hotmail.com>
2021-12-02 18:09:49 +0100

    Source to GitHub

--- build.bat
+++
@@ -1,5 +0,0 @@
-cd src
-set CC=gcc
-mingw32-make MINGW=1 WINDOWSHELL=1 SDL=1 NOOBJDUMP=1
-cd ..
-copy F:\srb29chaos\bin\Mingw\Release\srb2win.exe E:\SRB2\srb2chaos.exe
--- build.sh
+++
@@ -1,4 +0,0 @@
-export LIBGME_CFLAGS=
-export LIBGME_LDFLAGS=-lgme
-make -C src/ LINUX64=1
-sudo cp /home/zenya/srb29server/SRB2/bin/Linux64/Release/lsdl2srb2 /usr/games/SRB2/srb2server
